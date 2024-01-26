c8e0204c5204c6e7ab3d54db1e7631b0f772c765
Eleshghkooh <elnaz.eshghkooh@students.iaac.net>
2024-01-26 15:09:44 +0100

    Added requested changes for the page

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,107 @@
-<!-- the script is where the js code goes -->
-<script>
-import { ref } from "vue" // you need this to use ref()
-
-
-</script>
-
-
-
-<!-- the template is where the html code goes -->
 <template>
+    <div id="navbar" class="container">  
+        <div id="title">Elnaz's website</div>
+    </div>
 
-
-
+    <div id="flex">
+       <div id="sidebar" class="container">
+           Sidebar
+           <div>
+               <input type="text" v-model="selector" placeholder="#">
+               <input type="text" v-model="color" placeholder="Color">
+               <button @click="changeColor">Change Color</button>
+           </div>
+           
+           <div>
+               <input type="text" v-model="newText" placeholder="New Text">
+               <button @click="updateMainText">Change Text</button>
+           </div>
+       </div>
+       <div id="main" class="container">
+           {{ text }}
+       </div>
+   </div>
 </template>
 
+<script setup>
+import { ref } from 'vue';
 
+const text = ref('Elnaz'); 
+const newText = ref(''); 
 
-<!-- style is where the css code goes -->
+const selector = ref('');
+const color = ref('');
+
+function updateMainText() {
+    text.value = newText.value;
+}
+
+function changeColor() {
+    const elements = document.querySelectorAll(selector.value);
+    elements.forEach(el => {
+        el.style.backgroundColor = color.value; 
+    });
+}
+</script>
+
 <style scoped>
 
+</style>
 
+
+<style scoped>
+html {
+    background-color: rgb(59, 59, 59); 
+    color: white;
+}
+
+body {
+    margin: 0;
+}
+
+div {
+    box-sizing: border-box;
+}
+
+img {
+    height: 100%;
+    width: auto;
+}
+
+#navbar {
+    height: 50px;
+    border: 1px dotted red; 
+}
+
+#flex {
+    display: flex;
+    height: calc(100vh - 50px);
+}
+
+#sidebar {
+    width: 30%;
+    border: 1px dotted blue; 
+}
+
+#main {
+    width: 70%;
+    border: 1px dotted green; 
+}
+
+#title {
+    width: 70%;
+    float: left;
+}
+
+#logo {
+    width: 30%;
+    height: 100%;
+    float: right;
+    text-align: right;
+}
+
+.container {
+    border-style: dotted;
+    border-width: 1px;
+}
 </style>
